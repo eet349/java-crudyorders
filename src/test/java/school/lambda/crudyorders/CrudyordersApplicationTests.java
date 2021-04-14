b9da0d8a765c9f07c150f58536eb9950d6fdfccf
eet349 <51262590+eet349@users.noreply.github.com>
2021-04-14 16:15:27 -0500

    Initial commit.

---
+++ src/test/java/school/lambda/crudyorders/CrudyordersApplicationTests.java
@@ -0,0 +1,13 @@
+package school.lambda.crudyorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudyordersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
